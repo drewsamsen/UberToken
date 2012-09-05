da5f39385f6929db0a1b1f9959f9dffbfd8d8e6c
Drew Skwarcan <drew@weblogicnow.com>
2012-09-04 21:01:18 -0700

    New rails app using Postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UberToken::Application.config.secret_token = 'c19cf1f2d18578e677bedb572692a442cab160702ab573c2f7fba6e05f7a1dff905331b22f5cd7819475b2b771673133c8f271b9b5950c1aee3b4305a0cf925f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+UberToken::Application.config.session_store :cookie_store, key: '_UberToken_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# UberToken::Application.config.session_store :active_record_store
