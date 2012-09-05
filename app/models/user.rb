fb4d7e9b4abba63ec12ac5c45b1d03c47acc542e
Drew Skwarcan <drew@weblogicnow.com>
2012-09-05 13:06:35 -0700

    Added user image upload with paperclip gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 	validates :name, :presence => true
 	has_many :assignments
 	has_many :projects, :through => :assignments
+	attr_accessible :avatar, :name
+	has_attached_file :avatar, :styles => { :medium => "40x40>", :thumb => "16x16>" }
 
 	def self.tokens(query)
 		users = where("name ilike ?", "%#{query}%") 
